8044c0426f8d36bb2c25f530ccf7c7888eaf1316
JohnKamaujk <johnjkkamau@gmail.com>
2024-03-16 22:44:40 +0300

    :sparkles:feat:Home page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,3 +1,6 @@
+import landingImage from "../assets/landing.png";
+import appDownloadImage from "../assets/appDownload.png";
+
 const HomePage = () => {
   return (
     <div className="flex flex-col gap-12">
@@ -6,6 +9,19 @@ const HomePage = () => {
           Savor the Flavor Today!
         </h1>
         <span className="text-xl">Food is just a click away!</span>
+      </div>
+      <div className="grid md:grid-cols-2 gap-5">
+        <img src={landingImage} />
+        <div className="flex flex-col items-center justify-center gap-4 text-center">
+          <span className="font-bold text-3xl tracking-tighter">
+            Get your takeaway orders in a flash!
+          </span>
+          <span>
+            Download the FoodieExpress App for quicker orders and personalized
+            recommendations!
+          </span>
+          <img src={appDownloadImage} />
+        </div>
       </div>
     </div>
   );
