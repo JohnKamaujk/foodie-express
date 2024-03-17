053bc4aa72512a2499379ae563bebfd28f1be689
JohnKamaujk <johnjkkamau@gmail.com>
2024-03-17 21:29:48 +0300

    :sparkles:feat:User Routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,9 +12,8 @@ const PORT = 5000;
 app.use(express.json());
 app.use(cors());
 
-app.get("/", async (req: Request, res: Response) => {
-  res.json({ message: "Hello there!" });
-});
+
+app.use("/api/my/user", myUserRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
