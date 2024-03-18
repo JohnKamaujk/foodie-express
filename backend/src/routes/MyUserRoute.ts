ca80ab34d530c4ec8d40a1102197d3cdd3e58ee4
JohnKamaujk <johnjkkamau@gmail.com>
2024-03-18 21:59:39 +0300

    :sparkles:feat:validate user req payload

--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import MyUserController from "../controllers/MyUserController";
 import { jwtCheck, jwtParse } from "../middlewares/auth";
+import { validateMyUserRequest } from "../middlewares/validation";
 
 const router = express.Router();
 
 // /api/my/user
 router.post("/", jwtCheck, MyUserController.createCurrentUser);
-router.put("/", jwtCheck, jwtParse, MyUserController.updateCurrentUser);
+router.put(
+  "/",
+  jwtCheck,
+  jwtParse,
+  validateMyUserRequest,
+  MyUserController.updateCurrentUser
+);
 export default router;
