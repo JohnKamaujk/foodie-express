c46855bc02cddd8887464f90424ce30e71e1ecfe
JohnKamaujk <johnjkkamau@gmail.com>
2024-03-19 16:42:07 +0300

    :construction_worker:build:preparing API build

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-import myUserRoute from "../src/routes/MyUserRoute"
+import myUserRoute from "./routes/MyUserRoute"
 
 mongoose.connect(process.env.MONGODB_URI as string).then(() => {
   console.log("Connected to database successfully");
@@ -13,6 +13,9 @@ const PORT = 5000;
 app.use(express.json());
 app.use(cors());
 
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health OK!" });
+});
 
 app.use("/api/my/user", myUserRoute);
 
