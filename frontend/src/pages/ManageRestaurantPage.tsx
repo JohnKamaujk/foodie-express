109c21c92efff39e0b6256d05e923bd65e6788f9
JohnKamaujk <johnjkkamau@gmail.com>
2024-03-20 22:09:30 +0300

    :sparkles:feat:Create restaurant hook

--- frontend/src/pages/ManageRestaurantPage.tsx
+++ frontend/src/pages/ManageRestaurantPage.tsx
@@ -1,7 +1,15 @@
+import { useCreateMyRestaurant } from "@/api/MyRestaurantApi";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 const ManageRestaurantPage = () => {
-  return <ManageRestaurantForm />;
+  const { createRestaurant, isLoading: isCreateLoading } =
+    useCreateMyRestaurant();
+  return (
+    <ManageRestaurantForm
+      onSave={createRestaurant}
+      isLoading={isCreateLoading}
+    />
+  );
 };
 
 export default ManageRestaurantPage;
