cb83ee92dc0c5a6f1022b6f1c0a8571a1e5c28ad
JohnKamaujk <johnjkkamau@gmail.com>
2024-03-22 11:44:46 +0300

    :sparkles:feat:search state tracking

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,10 +1,19 @@
 import { useSearchRestaurants } from "@/api/RestaurantApi";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+
+export type SearchState = {
+  searchQuery: string;
+};
 
 const SearchPage = () => {
   const { city } = useParams();
 
   const { results, isLoading } = useSearchRestaurants(city);
+  
+  const [searchState, setSearchState] = useState<SearchState>({
+    searchQuery: "",
+  });
 
   if (isLoading) {
     <span>Loading ...</span>;
