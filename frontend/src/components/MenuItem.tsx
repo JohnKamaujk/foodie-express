ab651628b9d54fc1ba3dd6b43f262aee7bcaab4c
JohnKamaujk <johnjkkamau@gmail.com>
2024-03-23 13:37:14 +0300

    :sparkles:feat:menu item card component

--- frontend/src/components/MenuItem.tsx
+++ frontend/src/components/MenuItem.tsx
@@ -12,7 +12,7 @@ const MenuItem = ({ menuItem }: Props) => {
         <CardTitle>{menuItem.name}</CardTitle>
       </CardHeader>
       <CardContent className="font-bold">
-        £{(menuItem.price / 100).toFixed(2)}
+        ${(menuItem.price / 100).toFixed(2)}
       </CardContent>
     </Card>
   );
