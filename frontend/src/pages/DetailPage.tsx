219b130dad7faca3d5a53f301ad6dca062c33335
JohnKamaujk <johnjkkamau@gmail.com>
2024-03-23 14:03:01 +0300

    :sparkles:feat:order card component

--- frontend/src/pages/DetailPage.tsx
+++ frontend/src/pages/DetailPage.tsx
@@ -1,12 +1,23 @@
 import { useGetRestaurant } from "@/api/RestaurantApi";
 import MenuItem from "@/components/MenuItem";
+import OrderSummary from "@/components/OrderSummary";
 import RestaurantInfo from "@/components/RestaurantInfo";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Card, CardFooter } from "@/components/ui/card";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+
+export type CartItem = {
+  _id: string;
+  name: string;
+  price: number;
+  quantity: number;
+};
 
 const DetailPage = () => {
   const { restaurantId } = useParams();
   const { restaurant, isLoading } = useGetRestaurant(restaurantId);
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
 
   if (isLoading || !restaurant) {
     return "Loading...";
@@ -27,6 +38,11 @@ const DetailPage = () => {
           {restaurant.menuItems.map((menuItem) => (
             <MenuItem menuItem={menuItem} />
           ))}
+        </div>
+        <div>
+          <Card>
+            <OrderSummary restaurant={restaurant} cartItems={cartItems} />
+          </Card>
         </div>
       </div>
     </div>
